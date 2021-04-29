6301ca96e90013455342bc18652fdcabc2caed1e
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-04-29 14:40:20 +0100

    Create files

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { useState } from 'react'
+import TodoList from './TodoListcomponents/TodoList'
+//stores all states of our Todos
+// Used state returns an array
+function App() {
+  const [todos, setTodos] = useState(['Todo 1','Todo 2'])//Object destructuring, the first variable todos, represents every single one of the todos inside of our todo state,
+                                        // and the 2nd is the function we call to update the todos
+  return (// you cannot put two jsx elements next to each as it can only return one thing, wrapping inside a fragment '<>' allows us to return one thing which contains two jsx elements
+    <> 
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
+
+{/* <TodoList /> This renders our component in the browsers */}
---
+++ my-app/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+
+const Heading = () =>(
+
+    <div>
+        <h1>Welcome to REACT WORLD!</h1>
+        <h2>This will be an amazing trip!</h2>
+    </div>
+        
+)
+
+export default Heading
